37bf677223d453131be4c513b4c72fd75457ce1f
Daniel Ockiya <annonbytes@gmail.com>
2021-04-30 23:51:52 +0100

    Added some boiler files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
   console.log(`Server running on https://localhost:${port}`);
